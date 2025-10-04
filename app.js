62046a7648414f5709c368550eb3206d41f6989f
KushagraJaiswar02 <kush.8q@gmail.com>
2025-10-04 10:56:35 +0530

    signup login

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use((req, res, next) => {
 
 // --- ROUTE IMPORTS (Controller) ---
 // ... (Route imports remain the same)
-// app.use('/', require('./routes/index'));
+app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/user'));
 // app.use('/expenses', require('./routes/expense'));
 
